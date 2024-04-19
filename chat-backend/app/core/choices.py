db774098b5834a0e54f013a7f81331c60ae12075
Kamrul Hasan <kamrul.h456@gmail.com>
2024-04-20 00:36:26 +0600

    initial project setup

---
+++ chat-backend/app/core/choices.py
@@ -0,0 +1,16 @@
+from django.db.models import TextChoices
+
+
+class UserKind(TextChoices):
+    ADMIN = "ADMIN", "Admin"
+    DEVELOPER = "DEVELOPER", "Developer"
+    SUPER_ADMIN = "SUPER_ADMIN", "Super Admin"
+    OTHER = "OTHER", "Other"
+    UNDEFINED = "UNDEFINED", "Undefined"
+
+
+class UserGender(TextChoices):
+    FEMALE = "FEMALE", "Female"
+    MALE = "MALE", "Male"
+    UNKNOWN = "UNKNOWN", "Unknown"
+    OTHER = "OTHER", "Other"

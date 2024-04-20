7eea8e9f6e4f198692924c3df41ab267450886ea
Kamrul Hasan <kamrul.h456@gmail.com>
2024-04-20 15:05:54 +0600

    user login singup and navigation added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
+import Login from './Components/Login';
+import Navigate from './Components/Navigate';
 import Register from './Components/Register';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className='container'>
-      <h1 className='text-center'>Chatting Apps</h1>
-      <hr />
-      <Register />
-    </div>
+    <BrowserRouter>
+      <Navigate />
+      <Routes>
+        <Route path='/login' element={<Login />}></Route>
+        <Route path='/register' element={<Register />}></Route>
+        {/* <Route path="*" element={<h1>404 Not Found</h1>} /> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/Components/Login.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+import { TextField, Button } from "@mui/material"
+
+
+
+export default function Login() {
+    const BASE_URL = "http://127.0.0.1:8000/api/v1";
+    const [formData, setFormData] = useState({
+        "email": "",
+        "password": "",
+    })
+    const handleFormSubmit = () => {
+        fetch(`${BASE_URL}/users/login`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(formData)
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                alert(`Welcome login successfull!!!`)
+                // Reset form data after successful Login
+                setFormData({
+                    "email": "",
+                    "password": "",
+                });
+            })
+            .catch(err => {
+                console.log(err);
+                alert(`Error logging user ${err}`);
+            })
+    }
+    return (
+        <div className='text-center'>
+            <h3>User Login</h3>
+            <hr />
+            <div className='mt-3'>
+                <TextField id="email" label="Email" variant="outlined" onChange={e => setFormData({ ...formData, email: e.target.value })} />
+            </div>
+            <div className='mt-3'>
+                <TextField id="password" label="Password" type="password" variant="outlined" onChange={e => setFormData({ ...formData, password: e.target.value })} />
+            </div>
+            <div className="mt-3">
+                <Button variant='contained' onClick={handleFormSubmit}>Login</Button>
+            </div>
+        </div>
+    )
+}
